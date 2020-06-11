504d5a9da2d20f5ba26438c510de36a59543cb07
Eljus Sipovski <eljus.sipovski@wsp.com>
2020-06-11 10:39:07 +0100

    allure step added for reports

--- src/main/java/com/postcodes/api/assertions/AssertableResponse.java
+++ src/main/java/com/postcodes/api/assertions/AssertableResponse.java
@@ -1,6 +1,7 @@
 package com.postcodes.api.assertions;
 
 import com.postcodes.api.conditions.Condition;
+import io.qameta.allure.Step;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -12,6 +13,7 @@ public class AssertableResponse {
     private final CloseableHttpResponse response;
     private final JSONObject responseJson;
 
+    @Step("api response should have {condition}")
     public AssertableResponse shouldHave(Condition condition) {
         log.info("About to check condition {}", condition);
         condition.check(response, responseJson);
